cf4f16f7623d5fe760c63df25a56c07f3568d0cf
Sonford <sonfordonyango@gmail.com>
2016-01-14 17:20:22 +0300

    lesson two committed

---
+++ resources/views/contact/index.blade.php
@@ -0,0 +1,15 @@
+<html>
+<head>
+    <title>Laravel</title>
+
+
+</head>
+<body>
+<div class="container">
+    <div class="content">
+        <div class="title">Laravel 5</div>
+        <div class="quote">Sonford </div>
+    </div>
+</div>
+</body>
+</html>
